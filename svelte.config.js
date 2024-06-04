1c4776b88153fc54b3b9f9c585e5585161cd5714
Ehab-24 <ehabs1775@gmail.com>
2024-06-04 18:17:04 +0500

    some

--- svelte.config.js
+++ svelte.config.js
@@ -11,7 +11,18 @@ const config = {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
+		adapter: adapter(),
+    vite: {
+      optimizeDeps: {
+        include: ['svelte-kit']
+      },
+      server: {
+        // Modify the Content Security Policy (CSP) headers
+        headers: {
+          'Content-Security-Policy': "default-src 'self' http: https: data: 'unsafe-inline' 'unsafe-eval'",
+        },
+      },
+    },
 	}
 };
 
